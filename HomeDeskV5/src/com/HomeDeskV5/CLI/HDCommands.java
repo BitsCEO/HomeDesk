7082d9748f8d2469ac782a1cdccbe4cd2c23ea2a
lefto <lefto@DESKTOP-TP65BGM>
2025-09-12 15:53:24 -0500

    Update HDShell. Ready to begin testing.

---
+++ HomeDeskV5/src/com/HomeDeskV5/CLI/HDCommands.java
@@ -0,0 +1,36 @@
+package com.HomeDeskV5.CLI;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Spec;
+
+@Command(name = "",
+description = "Top-level shell command",
+mixinStandardHelpOptions = true, 
+subcommands = {
+		CreateEntityCMD.class,
+		NavigateCMD.class,
+		ShowEntityCMD.class
+})
+class HDCommands implements Runnable {
+	
+	@Spec
+	CommandSpec spec;
+	
+	@Option(names = {"-h", "--help"}, usageHelp = true, description = "Display help")
+	boolean helpRequested; 
+
+	@Override
+	public void run() {
+		
+		if (spec.commandLine().getSubcommands().isEmpty() || helpRequested) {
+			spec.commandLine().usage(System.out);
+		} else {
+			System.out.println()
+		}
+		
+		
+	}
+	
+}
---
+++ HomeDeskV5/src/com/HomeDeskV5/CLI/HDShell.java
@@ -0,0 +1,109 @@
+package com.HomeDeskV5.CLI;
+
+import java.io.IOException;
+
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.ParsedLine;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import com.HomeDeskV5.taskDirectory.Folder;
+import com.HomeDeskV5.taskDirectory.HDPath;
+
+import picocli.CommandLine;
+import picocli.CommandLine.InitializationException;
+import picocli.shell.jline3.PicocliCommands;
+
+public class HDShell {
+	
+	// ENV fields
+	private Folder directory;
+	private HDPath cursor;
+	
+	// CLI fields
+	private CommandLine commandLine;
+	private Terminal terminal;
+	private PicocliCommands picocliCommands;
+	private LineReader reader;
+	
+	public HDShell() {
+		this.directory = new Folder();
+		this.cursor = this.directory.getPath();
+		
+		if (initializeShell()) {
+			startInputLoop();
+		} else {
+			System.out.println("HDShell initialization failed!");
+		}
+		
+		
+	}
+	
+	private void startInputLoop() {
+		
+		while(true) {
+			String input = null;
+			
+			try {
+				input = reader.readLine(prompt());
+				ParsedLine parsedLine = reader.getParser().parse(input, 0);
+				String[] args = parsedLine.words().toArray(new String[0]);
+				
+				if (args.length == 0) {
+					continue;
+				}
+				
+				commandLine.execute(args);
+				
+			} catch (UserInterruptException uie) {
+				// TODO Handle UserInterruptException in interactive loop
+			} catch (EndOfFileException eofe) {
+				return;
+			} catch (Exception e) {
+				commandLine.getErr().println(e.getMessage());
+			}
+			
+		}
+		
+	}
+	
+	private boolean initializeShell() {
+		try {
+			
+			// Initialize terminal
+			terminal = TerminalBuilder.builder().build();
+			
+			// Initialize Command Line Interface, which are the commands that connect typing input
+			//  to programatic behavior. Link this to the top-level command
+			commandLine = new CommandLine(new HDCommands());
+			
+			// Prepare the command line, loaded with the cli, to be plugged into JLine terminal
+			picocliCommands = new PicocliCommands(commandLine);
+			
+			// Create a line reader to interface the terminal (input/output) with the commands
+			reader = LineReaderBuilder.builder()
+					.terminal(terminal)
+					.completer(picocliCommands.compileCompleters())
+					.build();
+			
+		} catch (InitializationException ie) {
+			commandLine.getErr().println(ie.getMessage());
+			
+			return false;
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private String prompt() {
+		return String.format("{HD}%s>", cursor.toString());
+	}
+	
+}
